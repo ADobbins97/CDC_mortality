fa025745ed21d178deb059037197e5193bd50cf3
Alexandra Dobbins <Alexandra@dhcp-10-250-27-222.harvard.edu>
2019-05-09 22:34:03 -0400

    data & app

--- Lyme_Disease_Incidence_USA/app.R
+++ Lyme_Disease_Incidence_USA/app.R
@@ -14,24 +14,9 @@ library(shiny)
 library(shinyWidgets)
 library(shinythemes)
 library(tidyverse)
+library(plotly)
 
-lyme <- read_csv("LD-Case-Counts-by-County-00-17.csv") %>%
-  clean_names()
-
-ca_county <- read_csv("infectious-disease-cases-by-county-year-and-sex-2-27-19.csv") %>%
-  clean_names() %>%
-  filter(disease == "Lyme Disease")
-
-co_nymph <- read_csv("Deer_Tick_Surveillance__Nymphs__May_to_Sept__excluding_Powassan_virus__Beginning_2008.csv") %>%
-  clean_names() %>%
-  filter(county_centroid != "40 6546") %>%
-  select(year,
-    "county_name" = county, total_ticks_collected, tick_population_density, b_burgdorferi_percent,
-    a_phagocytophilum_percent, b_microti_percent, b_miyamotoi_percent, county_centroid
-  )
-
-co_adult <- read_csv("Deer_Tick_Surveillance__Adults__Oct_to_Dec__excluding_Powassan_virus__Beginning_2008.csv") %>%
-  clean_names()
+read_rds("cdc_worked_data.rds")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
@@ -39,7 +24,7 @@ ui <- navbarPage(
   theme = shinytheme("cosmo"),
 
   tabPanel(
-    "Main",
+    "Home",
     fluidPage(
       fluidRow(
         h1("About This Project"),
@@ -50,13 +35,13 @@ ui <- navbarPage(
         br(),
         p("However, symptoms quickly progress beyond the flu-like symptoms and the disease can be fully debilitating. Untreated, it will spread to one's joints, heart, and nervous system and cause a multitude of side effects: 
           Extreme fatigue, memory loss, mood swings, depression, anxiety, headaches, muscle fatigue, bone pain, abdominal disruption, bloating, digestive issues, loss of appetite, facial swelling, thyroid disease, and many more.
-          It can often be difficult to identify someone has lyme in the absence of visible physical markers. Thus, the data here is only the **identified likely** cases which are reported to the CDC.")
+          It can often be difficult to identify someone has lyme in the absence of visible physical markers. Thus, the data here is only the identified likely cases which are reported to the CDC.")
       )
     )
   ),
 
   tabPanel(
-    "State: New York",
+    "View Interactive Diagnoses",
     fluidPage(
       fluidRow(
         titlePanel("Tick Collection in New York from 2008 to 2017"),
@@ -71,27 +56,64 @@ ui <- navbarPage(
           # Show a plot of the generated distribution
         )
       )
-    )
-  ),
+      )
+    ),
 
-  tabPanel(
-    "National",
-    fluidPage(
-      fluidRow(
-        h1("Reported Cases of Lyme Disease per State, from 2000 to 2017"),
-        br(),
-        sidebarLayout(
-          sidebarPanel(
-            mainPanel(plotOutput("USMap", width = 500, height = 500))
-          ),
-          position = "left",
-          pickerInput("year", "Year", choices = c("2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"), selected = c("2000"), options = list(`actions-box` = TRUE), multiple = FALSE)
-          # Show a plot of the generated distribution
+      tabPanel(
+        "View Interactive Diagnoses",
+        fluidPage(
+          fluidRow(
+            h1("Reported Cases of Lyme Disease per State, from 2000 to 2017"),
+            br(),
+            sidebarLayout(
+              sidebarPanel(
+                mainPanel(plotOutput("USMap_year", width = 500, height = 500))),
+                
+              position = "left",
+              pickerInput("year", "Year", choices = c("2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"), selected = c("2000"), options = list(`actions-box` = TRUE), multiple = FALSE)
+              )))),
+              # Show a plot of the generated distribution
+              
+              tabPanel(
+                "View Interactive Diagnoses",
+                fluidPage(
+                  fluidRow(
+                    h1("Reported Cases of Lyme Disease per State, from 2000 to 2017"),
+                    br(),
+                    sidebarLayout(
+              sidebarPanel(
+                mainPanel(plotOutput("USMap", width = 500, height = 500))),
+          
+            position = "left",
+            pickerInput("year", "Year", choices = c("2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"), selected = c("2000"), options = list(`actions-box` = TRUE), multiple = FALSE)
+            # Show a plot of the generated distribution
+          )
+        )
+      )
+    ),
+
+      tabPanel(
+        "Age of Diagnoses",
+        fluidPage(
+          fluidRow(
+            titlePanel("Cases of Lyme by Age from 2010 to 2017"),
+            br(),
+            # Sidebar with a slider input for number of bins
+            sidebarLayout(
+              sidebarPanel(
+                mainPanel(plotOutput("Age", width = 500, height = 500))
+              ),
+              position = "left",
+              pickerInput("year", "Year", choices = c("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"), selected = c("2017"), options = list(`actions-box` = TRUE), multiple = FALSE)
+              # Show a plot of the generated distribution
+            )
+          )
         )
       )
     )
-  )
-)
+  
+
+
 
 
 
@@ -99,22 +121,13 @@ ui <- navbarPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   output$USMap <- renderPlot({
-    state_cases <-
-      lyme %>%
-      group_by(stname) %>%
-      summarize("2000" = sum(cases2000), "2001" = sum(cases2001), "2002" = sum(cases2002), "2003" = sum(cases2003), "2004" = sum(cases2004), "2005" = sum(cases2005), "2006" = sum(cases2006), "2007" = sum(cases2007), "2008" = sum(cases2008), "2009" = sum(cases2009), "2010" = sum(cases2010), "2011" = sum(cases2011), "2012" = sum(cases2012), "2013" = sum(cases2013), "2014" = sum(cases2014), "2015" = sum(cases2015), "2016" = sum(cases2016), "2017" = sum(cases2017), sum_all = sum(c(cases2000, cases2001, cases2002, cases2003, cases2004, cases2005, cases2006, cases2007, cases2008, cases2009, cases2010, cases2011, cases2012, cases2013, cases2014, cases2015))) %>%
-      arrange(desc(sum_all)) %>%
-      select(stname, "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017")
-
-    state_cases <- gather(state_cases, "2000", "2001", "2002", "2003", "2004", "2005", "sum_2006", "sum_2007", "sum_2008", "sum_2009", "sum_2010", "sum_2011", "sum_2012", "sum_2013", "sum_2014", "sum_2015", "sum_2016", "sum_2017",
-      key = "year", value = "cases"
-    )
-
-    state_cases %>%
+    x1 <- urbnmapr::states
+    y1 <- state_cases
+    left_join(x1, y1, by = "state_name") %>%
       ggplot() +
       geom_polygon(
-        data = urbnmapr::states, mapping = aes(x = long, y = lat, group = group),
-        fill = "#a2d4ec", color = "white"
+        mapping = aes(x = long, y = lat, group = group, fill = cases),
+        color = "white"
       ) +
       geom_text(
         data = get_urbn_labels(map = "states"), aes(x = long, lat, label = state_abbv),
@@ -123,11 +136,28 @@ server <- function(input, output) {
       coord_map(projection = "albers", lat0 = 39, lat1 = 45)
   })
 
-  output$NYPlot <- renderPlot({
+  output$USMap_year <- renderPlot({
+    x1 <- urbnmapr::states
+    y1 <- state_cases %>% filter(year == "input$year")
+    left_join(x1, y1, by = "state_name") %>%
+      ggplot() +
+      geom_polygon(
+        mapping = aes(x = long, y = lat, group = group, fill = cases),
+        color = "white"
+      ) +
+      geom_text(
+        data = get_urbn_labels(map = "states"), aes(x = long, lat, label = state_abbv),
+        size = 2
+      ) +
+      coord_map(projection = "albers", lat0 = 39, lat1 = 45)
+  })
+
+  output$NYPlot <- renderPlotly({
     # need to layer this so outline of new york doesn't disappear
     x <- urbn_data
     y <- nymph_tick_number %>% filter(year == input$year) %>% select(county_name, total_ticks)
-    left_join(x, y, by = "county_name") %>%
+    ny_plot <-
+      left_join(x, y, by = "county_name") %>%
       ggplot(mapping = aes(long, lat, group = group, fill = total_ticks)) +
       geom_polygon(color = "white", size = .25) +
       coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
@@ -136,6 +166,16 @@ server <- function(input, output) {
         legend.key.width = unit(.5, "in")
       ) +
       labs(fill = "Ticks Collected")
+    
+    ggplotly(p = ny_plot, width = 500, height = 500)
+  })
+
+  output$Age <- renderPlotly({
+    year_data %>%
+      select(year, age, cases) %>% 
+      filter(year == "input$year") %>%
+      ggplot(mapping = aes(x = age, y = cases)) +
+      geom_col()
   })
 }
 
