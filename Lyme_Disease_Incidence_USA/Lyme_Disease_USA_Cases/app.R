658882a85b4ff5eef4556b8e291bc3342e3c6c8f
Alexandra Dobbins <Alexandra@Alexandras-MacBook-Pro-5.local>
2019-05-10 22:58:17 -0400

    adding body text

--- Lyme_Disease_Incidence_USA/Lyme_Disease_USA_Cases/app.R
+++ Lyme_Disease_Incidence_USA/Lyme_Disease_USA_Cases/app.R
@@ -19,7 +19,7 @@ library(maps)
 library(rgeos)
 library(viridis)
 
-#Open datasets to be used for rest of app
+# Open datasets to be used for rest of app
 urbn_data <-
   urbnmapr::counties %>%
   filter(state_abbv == "NY") %>%
@@ -28,52 +28,19 @@ urbn_data <-
 
 read_rds("rds_files /cdc_worked_data.rds")
 
-#Setting up visual aspect of app
+# Setting up visual aspect of app
 
 ui <- fluidPage(
   theme = shinytheme("cosmo"),
 
   navbarPage(
     "Lyme Disease in the United Staes: the historical perspective",
-    
-#
-##
-###
-##
-#
 
-
-    tabPanel(
-      "Home",
-      sidebarPanel(
-        #4,
-        wellPanel(
-          h3("About the Analyst"),
-          br(),
-          h5("Alexandra Dobbins is a junior at Harvard College.")
-        )
-      ),
-      mainPanel(
-        titlePanel(
-          h3("Lyme Disease: Statistics and History in the United States"),
-          h5(
-            p("Every year, health care providers around the country report likely cases to the CDC. They, in turn assimilate this information into reports and datasets which are available to the public. However, because states and organizations report independently, the types of data is not always consistent and therefore in this study, only New York will be analyzed on a state-level for the sake of continuity."),
-            br(),
-            p("Every year, up to 300,000 cases of Lyme disease are reported. The disease is caused by a bacterium called Borrelia burgdoferi. It is passed to humans by ticks, although not all types of ticks carry it. After being bitted a `bulls-eye` circle appears around the bite site and this is the first warning sign you might have contracted Lyme. The problem is that sometimes people do not find the bite, and think they have a cold -- fever, headache, fatigue."),
-            br(),
-            p("However, symptoms quickly progress beyond the flu-like symptoms and the disease can be fully debilitating. Untreated, it will spread to one's joints, heart, and nervous system and cause a multitude of side effects: 
-                                            Extreme fatigue, memory loss, mood swings, depression, anxiety, headaches, muscle fatigue, bone pain, abdominal disruption, bloating, digestive issues, loss of appetite, facial swelling, thyroid disease, and many more.
-                                            It can often be difficult to identify someone has lyme in the absence of visible physical markers. Thus, the data here is only the identified likely cases which are reported to the CDC.")
-          )
-        )
-      )
-    ),
-
-#
-##
-###
-##
-#
+    #
+    ##
+    ###
+    ##
+    #
 
     tabPanel(
       "National Trends",
@@ -90,30 +57,17 @@ ui <- fluidPage(
               "2008", "2009", "2010", "2011", "2012", "2013", "2014",
               "2015", "2016", "2017"
             ), selected = "2000", multiple = FALSE)
-          ),
-          box(
-            wellPanel(
-              h3("Where and When"),
-              h4("Reported Cases of Lyme Disease by State and Year"),
-              br(),
-              h5("Use the selection bar to choose the year and hover fro specific statistics")
-            ),
-            mainPanel(
-              h3("Total State-Reported Cases of Lyme Disease since 2010"),
-              h4("Organized by State and by Year"),
-              plotlyOutput("US_Reported_Yearly")
-            )
           )
         )
       )
     ),
 
-# 
-# #
-# ##
-# #
-# 
-    
+    #
+    # #
+    # ##
+    # #
+    #
+
 
     tabPanel(
       "Population Distributions",
@@ -136,11 +90,10 @@ ui <- fluidPage(
                 tabPanel(
                   "Age",
                   h4("Reported Cases of Lyme Disease by Age Group"),
-                  h5("Year: "), # make this reflect the year chosen?
                   br(),
                   plotOutput("Barplot_Age"),
                   br(),
-                  h5("insert brief observation of data")
+                  h5("Individuals between 40 and 64 are seen to be most affected by Lyme disease. In addition, the overall number of cases reported and recorded has risen steadily. This could be due to either an increased awareness and tendency to diagnose, or becaue of more actual cases. ")
                 ),
                 tabPanel(
                   "Race",
@@ -149,7 +102,7 @@ ui <- fluidPage(
                   br(),
                   plotOutput("Barplot_Race"),
                   br(),
-                  h5("insert brief observation about data")
+                  h5("This graph is on a log scale. White individuals are most affected by lyme disease, white Alskans and Pacific Islanders are extremely low. This is not unsurprising because Lyme Disease originated and is most concentrated in New England.")
                 ),
                 tabPanel(
                   "Gender",
@@ -158,7 +111,7 @@ ui <- fluidPage(
                   br(),
                   plotOutput("Barplot_Gender"),
                   br(),
-                  h5("insert brief observation about data")
+                  h5("Gender-based data shows a less consistent trend, but notice how in recent years there have been many more cases in women than in men.")
                 )
               )
             )
@@ -167,12 +120,12 @@ ui <- fluidPage(
       )
     ),
 
-    
-# 
-##
-###
-##
-#
+
+    #
+    ##
+    ###
+    ##
+    #
 
     tabPanel(
       "Biological Information",
@@ -194,140 +147,168 @@ ui <- fluidPage(
             mainPanel(
               tabsetPanel(
                 tabPanel(
-              h4("Occurrence of Four Types of Bacteria Found in Adult Ticks"),
-              h5("as identified off samples collected from varying parts of New York since 2000"),
-              br(),
-              plotlyOutput("NY_Bacteria_Nymph"),
-                br(),
-                h5("Brief description of the data observed"),
-              tabPanel(
-                h4("Occurrence of Four Types of Bacteria Found in Nymph Ticks"),
-                       h5("as identified off samples collected from varying parts of New York since 2000"),
-                       br(),
-                       plotlyOutput("NY_Bacteria_Adult"),
-                       br(),
-                       h5("Brief description of the data observed"))
+                  h4("Occurrence of Four Types of Bacteria Found in Adult Ticks"),
+                  h5("as identified off samples collected from varying parts of New York since 2000"),
+                  br(),
+                  plotlyOutput("NY_Bacteria_Nymph"),
+                  
+                  tabPanel(
+                    h4("Occurrence of Four Types of Bacteria Found in Nymph Ticks"),
+                    h5("as identified off samples collected from varying parts of New York since 2000"),
+                    br(),
+                    plotlyOutput("NY_Bacteria_Adult")
+                    
+                  )
+                )
               )
             )
           )
         )
-     )
-   )
-)))
+      )
+    ),
+
+    #
+    ##
+    ###
+    ##
+    #
+
+
+    tabPanel(
+      "About",
+      sidebarPanel(
+        # 4,
+        wellPanel(
+          h3("About the Analyst"),
+          br(),
+          h5("Alexandra Dobbins is a junior at Harvard College.")
+        )
+      ),
+      mainPanel(
+        titlePanel(
+          h3("Lyme Disease: Statistics and History in the United States"),
+          h5(
+            p("Every year, health care providers around the country report likely cases to the CDC. They, in turn assimilate this information into reports and datasets which are available to the public. However, because states and organizations report independently, the types of data is not always consistent and therefore in this study, only New York will be analyzed on a state-level for the sake of continuity."),
+            br(),
+            p("Every year, up to 300,000 cases of Lyme disease are reported. The disease is caused by a bacterium called Borrelia burgdoferi. It is passed to humans by ticks, although not all types of ticks carry it. After being bitted a `bulls-eye` circle appears around the bite site and this is the first warning sign you might have contracted Lyme. The problem is that sometimes people do not find the bite, and think they have a cold -- fever, headache, fatigue."),
+            br(),
+            p("However, symptoms quickly progress beyond the flu-like symptoms and the disease can be fully debilitating. Untreated, it will spread to one's joints, heart, and nervous system and cause a multitude of side effects: 
+          Extreme fatigue, memory loss, mood swings, depression, anxiety, headaches, muscle fatigue, bone pain, abdominal disruption, bloating, digestive issues, loss of appetite, facial swelling, thyroid disease, and many more.
+          It can often be difficult to identify someone has lyme in the absence of visible physical markers. Thus, the data here is only the identified likely cases which are reported to the CDC.")
+          )
+        )
+      )
+    )
+  )
+)
 
 
 server <- function(input, output) {
+  output$US_Reported_Static <- renderPlotly({
+    ggplot(data = shapes2) +
+      geom_sf(mapping = aes(fill = avg_cases)) +
+      labs(
+        fill = "Unit",
+        title = "USA Tick Data"
+      )
+  })
 
-output$US_Reported_Static <- renderPlotly({
- 
-  ggplot(data = shapes2) +
-    geom_sf(mapping = aes(fill = avg_cases))+
-    labs(fill = "Unit", 
-         title = "USA Tick Data")
-  
-})
+  output$US_Reported_Yearly <- renderPlotly({
+    shapes <- shapes %>% filter(year == "input$year")
 
-output$US_Reported_Yearly <- renderPlotly({
-  
-  shapes <- shapes %>%  filter(year == "input$year")
-  
-  us_cases_map <-
-    ggplot(data = shapes) +
-    geom_sf(mapping = aes(fill = cases))+
-    labs(fill = "Reported Cases", 
-         caption = "Source: CDC") +
-    xlab(" ") +
-    ylab(" ")
-  
-  us_cases_map %>% 
-    ggplotly(tooltip = "text") %>% 
-    style(hoverlabel = list(bgcolor = "white"), hoveron = "fill")
-  
-})
+    us_cases_map <-
+      ggplot(data = shapes) +
+      geom_sf(mapping = aes(fill = cases)) +
+      labs(
+        fill = "Reported Cases",
+        caption = "Source: CDC"
+      ) +
+      xlab(" ") +
+      ylab(" ")
+
+    # us_cases_map %>%
+    #   ggplotly(tooltip = "text") %>%
+    #   style(hoverlabel = list(bgcolor = "white"), hoveron = "fill")
+  })
 
 
-output$NY_Bacteria_Nymph <- renderPlot({
-  
-  x1 <- urbn_data
-  y1 <- ny_tick_nymphs %>% filter(year == "input$year")
-  
-  left_join(x1, y1, by = "county_name") %>%
-    ggplot(mapping = aes(long, lat, group = group, fill = total_ticks)) +
-    geom_polygon(color = "white", size = .25) +
-    coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
-    theme(
-      legend.title = element_text(),
-      legend.key.width = unit(.5, "in")
-    ) +
-    labs(fill = "Ticks Collected")
-  
-})
+  output$NY_Bacteria_Nymph <- renderPlot({
+    x1 <- urbn_data
+    y1 <- ny_tick_nymphs %>% filter(year == "input$year")
 
-output$NY_Bacteria_Nymph <- renderPlot({
-  
-  x2 <- urbn_data
-  y2 <- ny_adult %>%  filter(year == "input$year")
-  
-  left_join(x2, y2, by = "county_name") %>% 
-    ggplot(mapping = aes(long, lat, group = group, fill = total_ticks)) +
-    geom_polygon(color = "black", size = .25) +
-    coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
-    theme(legend.title = element_text(),
-          legend.key.width = unit(.5, "in")) +
-    labs(title = "",
-         fill = "Ticks Collected")
-  
-})
-  
+    left_join(x1, y1, by = "county_name") %>%
+      ggplot(mapping = aes(long, lat, group = group, fill = total_ticks)) +
+      geom_polygon(color = "white", size = .25) +
+      coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
+      theme(
+        legend.title = element_text(),
+        legend.key.width = unit(.5, "in")
+      ) +
+      labs(fill = "Ticks Collected")
+  })
 
-output$Barplot_Age <- renderPlot({
-  
- 
+  output$NY_Bacteria_Nymph <- renderPlot({
+    x2 <- urbn_data
+    y2 <- ny_adult %>% filter(year == "input$year")
+
+    left_join(x2, y2, by = "county_name") %>%
+      ggplot(mapping = aes(long, lat, group = group, fill = total_ticks)) +
+      geom_polygon(color = "black", size = .25) +
+      coord_map(projection = "albers", lat0 = 39, lat1 = 45) +
+      theme(
+        legend.title = element_text(),
+        legend.key.width = unit(.5, "in")
+      ) +
+      labs(
+        title = "",
+        fill = "Ticks Collected"
+      )
+  })
+
+
+  output$Barplot_Age <- renderPlot({
     year_data_age %>%
-    select(year, age, cases) %>%
-    filter(year == "input$year")
+      select(year, age, cases) %>%
+      filter(year == "input$year")
     ggplot() +
-    geom_col(mapping = aes(x = age, y = cases, fill = ..x..))+
-    coord_flip() +
-    xlab("Age") +
-    ylab("Reported Cases") +
-    labs(title = "National Reported Cases of Lyme Disease by Age Group", 
-         caption = "Source: CDC")
-  
-})
+      geom_col(mapping = aes(x = age, y = cases, fill = ..x..)) +
+      coord_flip() +
+      xlab("Age") +
+      ylab("Reported Cases") +
+      labs(
+        title = "National Reported Cases of Lyme Disease by Age Group",
+        caption = "Source: CDC"
+      )
+  })
 
-output$Barplot_Race <- renderPlot({
-  
-
+  output$Barplot_Race <- renderPlot({
     year_data_race %>%
-    select(year, race, cases) %>%
-    filter(year == "input$year") %>% 
-    ggplot() +
-    geom_col(mapping = aes(x = race, y = cases, fill = ..x..))+
-    coord_flip() +
-    xlab("Race") +
-    ylab("Reported Cases") +
-    labs(title = "National Reported Cases of Lyme Disease by Race", 
-         caption = "Source: CDC") 
-  
-})
+      select(year, race, cases) %>%
+      filter(year == "input$year") %>%
+      ggplot() +
+      geom_col(mapping = aes(x = race, y = cases, fill = ..x..)) +
+      coord_flip() +
+      xlab("Race") +
+      ylab("Reported Cases") +
+      labs(
+        title = "National Reported Cases of Lyme Disease by Race",
+        caption = "Source: CDC"
+      )
+  })
 
-output$Barplot_Gender <- renderPlot({
-  
-
+  output$Barplot_Gender <- renderPlot({
     year_data_gender %>%
-    filter(year == "input$year") %>% 
-    select(year, gender, cases) %>%
-    ggplot() +
-    geom_col(mapping = aes(x = gender, y = cases, fill = ..x..))+
-    xlab("Gender") +
-    ylab("Reported Cases") +
-    labs(title = "National Reported Cases of Lyme Disease by Gender", 
-         caption = "Source: CDC")
-  
-})
-  
-  
+      filter(year == "input$year") %>%
+      select(year, gender, cases) %>%
+      ggplot() +
+      geom_col(mapping = aes(x = gender, y = cases, fill = ..x..)) +
+      xlab("Gender") +
+      ylab("Reported Cases") +
+      labs(
+        title = "National Reported Cases of Lyme Disease by Gender",
+        caption = "Source: CDC"
+      )
+  })
 }
 
 # Run the application
